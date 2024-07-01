beaf8b95b34a09015078a4a1927848cbb6dc85f3
psosnin <philipsosnin@gmail.com>
2024-07-01 16:18:03 +0100

    fix(examples): fullly connected model added an extra layer

--- examples/models/fully_connected.py
+++ examples/models/fully_connected.py
@@ -2,11 +2,12 @@
 
 import torch
 
+
 class FullyConnected(torch.nn.Sequential):
     def __init__(self, in_dim, out_dim, hidden_dim, hidden_lay):
         layers = [torch.nn.Linear(in_dim, hidden_dim)]
         layers.append(torch.nn.ReLU())
-        for _ in range(hidden_lay):
+        for _ in range(hidden_lay - 1):
             layers.append(torch.nn.Linear(hidden_dim, hidden_dim))
             layers.append(torch.nn.ReLU())
         layers.append(torch.nn.Linear(hidden_dim, out_dim))
